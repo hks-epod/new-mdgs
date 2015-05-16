048888d14778da8629c2ec3fd6c495dd540daaf7
Nils Enevoldsen <nils@wlonk.com>
2015-05-15 21:45:46 -0400

    Sculpt data and add a demonstration plot

--- new-mdgs.R
+++ new-mdgs.R
@@ -1,1 +1,30 @@
-data <- read.csv("MDG_Export_20150514_231243045.csv")
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+
+data <- 
+  read.csv("MDG_Export_20150514_231243045.csv", stringsAsFactors = FALSE) %>% 
+  tbl_df %>%
+  slice(1:40356) %>%
+  filter(MDG == "Y")
+  
+names(data) <-
+  gsub("X", "", names(data), fixed = TRUE)
+
+data$SeriesCode %<>%
+  factor(levels = data$SeriesCode[1:80], labels = data$Series[1:80])
+
+data$CountryCode %<>%
+  factor(levels = unique(data$CountryCode), labels = unique(data$Country))
+
+data %<>%
+  select(-Series,-Country,-MDG,-contains("Footnotes"),-contains("Type")) %>% 
+  gather(Year, Value, 3:28) %>% 
+  mutate(Year = extract_numeric(Year))
+#TODO: spread() on Value
+
+data %>%
+  filter(SeriesCode == "Mobile-cellular subscriptions per 100 inhabitants") %>% 
+  ggplot(aes(Year, Value)) +
+    geom_line(aes(color=CountryCode))
