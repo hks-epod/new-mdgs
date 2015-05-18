9a8e8c80e8f7b519023b5fca79d4b5f5e82200fa
Nils Enevoldsen <nils@wlonk.com>
2015-05-18 15:18:01 -0400

    Make plotting into function

--- new-mdgs.R
+++ new-mdgs.R
@@ -5,19 +5,32 @@ library(tidyr)
 library(magrittr)
 library(ggplot2)
 
-goals <- 
+goals <-
   read.csv("MDG_Export_20150514_231243045.csv", stringsAsFactors = FALSE) %>% 
   tbl_df %>%
-  slice(1:40356) %>%                      # Exclude footnotes
-  filter(MDG %>% equals("Y")) %>%
-  select(Country,Series,contains("X"))%>% # Years are named "X1990" etc.
-  gather(Year, Value, -(1:2)) %>% 
-  spread(Series, Value) %>% 
+  slice(1:40356) %>%                        # Chop off footnotes
+  filter(MDG == "Y") %>%                    # Filter out variables that aren't goals
+  select(Country,Series,contains("X"))%>%   # Keep only countries, goals, and years
+  gather(Year, Value, -(1:2)) %>%           # Make year into a variable (a column)
+  spread(Series, Value) %>%                 # Make goals into variables (columns)
+  mutate(Country = as.factor(Country)) %>% 
   mutate(Year = extract_numeric(Year))
 
-goals %>%
-  ggplot(aes(Year, `Fixed-telephone subscriptions per 100 inhabitants`)) %>%
-  add(geom_line(aes(color=Country))) %>% 
-  add(ggtitle('Fixed-telephone subscriptions per 100 inhabitants')) %>% 
-  add(theme(legend.position="none")) %>% 
-  print
+goal.names <- goals %>% colnames %>% inset(1:2, NA) # Store goal names with punctuation...
+
+colnames(goals) %<>% make.names                     # ...then standardize column titles
+
+plot.country.progress <-
+  . %>%
+  {
+    goal.colname <- colnames(goals)[[.]]
+    goal.name <- goal.names[[.]]
+    ggplot(goals, aes_string("Year", goal.colname)) %>%
+    add(geom_line(aes(color = Country))) %>% 
+    add(ggtitle(goal.name)) %>% 
+    add(labs(y = goal.name)) %>% 
+    add(theme(legend.position = "none")) %>% 
+    print
+  }
+
+plot.country.progress(3)
