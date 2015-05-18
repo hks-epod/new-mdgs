057cc26065bee13cbb6e558c0448559fec30cb3d
Nils Enevoldsen <nils@wlonk.com>
2015-05-17 23:51:58 -0400

    Put off factors for now

--- new-mdgs.R
+++ new-mdgs.R
@@ -7,21 +7,14 @@ data <-
   read.csv("MDG_Export_20150514_231243045.csv", stringsAsFactors = FALSE) %>% 
   tbl_df %>%
   slice(1:40356) %>%
-  filter(MDG == "Y")
-
-data$SeriesCode %<>%
-  factor(labels = data$Series[1:80])
-
-data$CountryCode %<>%
-  factor(labels = unique(data$Country))
-
-data %<>%
-  select(-Series,-Country,-MDG,-contains("Footnotes"),-contains("Type")) %>% 
-  gather(Year, Value, 3:28) %>% 
+  filter(MDG %>% equals("Y")) %>%
+  select(-MDG,-contains("Footnotes"),-contains("Type")) %>% 
+  gather(Year, Value, 5:30) %>% 
   mutate(Year = extract_numeric(Year))
 #TODO: spread() on Value
 
 data %>%
-  filter(SeriesCode == "Mobile-cellular subscriptions per 100 inhabitants") %>% 
-  ggplot(aes(Year, Value)) +
-    geom_line(aes(color=CountryCode))
+  filter(SeriesCode %>% equals("605")) %>% 
+  ggplot(aes(Year, Value)) %>%
+  add(geom_line(aes(color=CountryCode))) %>% 
+  print
